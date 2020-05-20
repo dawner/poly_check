d21752b4ce3a3d23788753c09177f4b05784d867
Dawn Richardson <dawn.richardson@abletech.co.nz>
2020-05-20 20:31:52 +1200

    Check address contained in random polygon

--- src/address_finder.js
+++ src/address_finder.js
@@ -1,11 +1,25 @@
 export function addressFinder() {
   const input = document.getElementById("searchTextField")
   const resultDisplay = document.getElementById("addressResult")
+  const polyCheckDisplay = document.getElementById("polyCheck")
 
   const options = { types: ["address"] }
   const autocomplete = new google.maps.places.Autocomplete(input, options)
 
-  let chosenLocation = null
+  let coordinates = [
+    { lat: -40, lng: 169 },
+    { lat: -42, lng: 169 },
+    { lat: -42, lng: 179 },
+    { lat: -40, lng: 179 },
+  ]
+
+  let polygon = new google.maps.Polygon({ paths: coordinates })
+
+  const isInsidePolygon = (lat, lng) => {
+    var latLng = new google.maps.LatLng(lat, lng)
+    return google.maps.geometry.poly.containsLocation(latLng, polygon)
+  }
+
   const updateSelectedPlace = () => {
     let place = autocomplete.getPlace()
     if (!place.geometry) {
@@ -13,13 +27,14 @@ export function addressFinder() {
       return
     }
 
-    chosenLocation = place.geometry.location
-    console.log(
-      `You selected: ${chosenLocation.lat()}, ${chosenLocation.lng()}`
-    )
+    let loc = place.geometry.location
+    let isInside = isInsidePolygon(loc.lat(), loc.lng())
+
+    console.log(`You selected: ${loc.lat()}, ${loc.lng()}`)
 
     input.value == ""
     resultDisplay.innerText = `Chosen: ${place.formatted_address}`
+    polyCheckDisplay.innerText = `Which ${isInside ? "IS" : "IS NOT"} inside the polygon`
   }
 
   autocomplete.addListener("place_changed", updateSelectedPlace)
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { addressFinder } from "./address_finder.js"
 const loadGoogle = () => {
   var script = document.createElement("script")
   script.type = "text/javascript"
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places,geometry`
   script.onload = addressFinder
   document.body.appendChild(script)
 }
