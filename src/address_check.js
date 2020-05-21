7233bcfcf305b18f88c1c00c5b931e8a24374189
Dawn Richardson <dawn.richardson@abletech.co.nz>
2020-05-21 16:24:19 +1200

    Display a map with marker and polygon

--- src/address_check.js
+++ src/address_check.js
@@ -1,16 +1,14 @@
-export function addressCheck(polygon) {
+export function addressCheck(map, mapPolygon) {
   const input = document.getElementById("searchTextField")
   const resultDisplay = document.getElementById("addressResult")
   const polyCheckDisplay = document.getElementById("polyCheck")
-  polyCheckDisplay.innerText = `Search for an address to see if it's inside the polygon: ${polygon.name}`
+  polyCheckDisplay.innerText = `Search for an address to see if it's inside the polygon: ${mapPolygon.name}`
 
   const options = { types: ["address"] }
   const autocomplete = new google.maps.places.Autocomplete(input, options)
+  let marker = null
 
-  let mapPolygon = new google.maps.Polygon({ paths: polygon.coordinates })
-
-  const isInsidePolygon = (lat, lng) => {
-    let latLng = new google.maps.LatLng(lat, lng)
+  const isInsidePolygon = (latLng) => {
     return google.maps.geometry.poly.containsLocation(latLng, mapPolygon)
   }
 
@@ -22,13 +20,19 @@ export function addressCheck(polygon) {
     }
 
     let loc = place.geometry.location
-    let isInside = isInsidePolygon(loc.lat(), loc.lng())
+    let latLng = new google.maps.LatLng(loc.lat(), loc.lng())
+
+    let isInside = isInsidePolygon(latLng)
+
+    marker && marker.setMap(null) // remove any old marker
+    marker = new google.maps.Marker({ position: latLng, map: map })
+    map.setCenter(latLng)
 
     console.log(`You selected: ${loc.lat()}, ${loc.lng()}`)
 
     input.value == ""
     resultDisplay.innerText = `Chosen: ${place.formatted_address}`
-    polyCheckDisplay.innerText = `Which ${isInside ? "IS" : "IS NOT"} inside the polygon: ${polygon.name}`
+    polyCheckDisplay.innerText = `Which ${isInside ? "IS" : "IS NOT"} inside the polygon: ${mapPolygon.name}`
   }
 
   autocomplete.addListener("place_changed", updateSelectedPlace)
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import { addressCheck } from "./address_check.js"
 const setup = () => {
   fetch("/polygon")
     .then((response) => response.json())
-    .then((data) => {
-      addressCheck(data)
+    .then((polygon) => {
+      let mapPolygon = new google.maps.Polygon({
+        paths: polygon.coordinates,
+        strokeColor: "#FF0000",
+        fillColor: "#FF0000",
+      })
+      let polyBounds = new google.maps.LatLngBounds()
+      mapPolygon.getPath().forEach(function (element, index) {
+        polyBounds.extend(element)
+      })
+
+      let map = new google.maps.Map(document.getElementById("map"), {
+        center: polyBounds.getCenter(),
+      })
+
+      mapPolygon.setMap(map)
+
+      map.fitBounds(polyBounds)
+      map.setZoom()
+
+      addressCheck(map, mapPolygon)
     })
     .catch((error) => {
       console.error("Error fetching polygon:", error)
